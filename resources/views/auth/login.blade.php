109313c2369db4931209ee6a6e47f4508ebafd01
twist.zheng <twist.zheng@coryphaei.com>
2016-06-15 23:01:31 +0800

    remove forget password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,7 +54,7 @@
                                     <i class="fa fa-btn fa-sign-in"></i>登录
                                 </button>
 
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">忘记密码?</a>
+                                {{--<a class="btn btn-link" href="{{ url('/password/reset') }}">忘记密码?</a>--}}
                             </div>
                         </div>
                     </form>
